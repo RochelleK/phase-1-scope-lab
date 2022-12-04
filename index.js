bced01e6f4b1b72650f85ca1c2d0d9d5f8338396
Rochelle Katzman <zelrochelle@gmail.com>
2022-12-03 22:48:25 -0500

    commit tree 1173da89fa347fe3f1da499a38388fde9d14b219

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    var bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'henri'
+
+function leastFavoriteCustomer(){
+    
+}
+
+function leastFavoriteCustomer() { leastFavoriteCustomer = 'henry'}
